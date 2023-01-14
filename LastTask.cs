771f795e51b34cce2fde1fada79a17053da3411f
Вересков Андрей <vereskov1985@mail.ru>
2023-01-14 19:57:38 +0300

    Thirth commit

--- LastTask.cs
+++ LastTask.cs
@@ -10,6 +10,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string otvet = "";
+            while (otvet.ToLower() != "yes" && otvet.ToLower() != "no")
+            {
+                Console.Write("Введёте данные вручную? (yes/no): ");
+                otvet = Console.ReadLine();
+            }
+
+            string[] massiveS = new string[] { };
+
+            if (otvet.ToLower() == "yes")
+            {
+                int n = InputN("Введите количество элементов массива: ");
+                massiveS = new string[n];
+                for (int i = 0; i < massiveS.Length; i++)
+                {
+                    Console.Write($" Введите {i + 1} строку: ");
+                    massiveS[i] = Console.ReadLine();
+                }
+            }
+            else
+            {
+                massiveS = new string[] { "hello", "2", "world", ":-)" };
+                Console.WriteLine($"{PrintM(massiveS)}");
+            }
+
+            int lengthLimit = 3;
+
+            int numbersItems = CheckM(massiveS, lengthLimit);
+
+            string[] newMassiveS = new string[numbersItems];
+
+            FillNewM(massiveS, newMassiveS, lengthLimit);
+
+            Console.WriteLine($"{PrintM(newMassiveS)}");
+
+
             void FillNewM(string[] oldArray, string[] newArray, int lengthLimit2)
             {
                 int temp = 0;
@@ -20,8 +56,7 @@ namespace ConsoleApp1
                         newArray[temp] = oldArray[i];
                         temp++;
                     }
-                }
-                
+                } 
             }
 
             int CheckM(string[] array, int lengthLimit2)
