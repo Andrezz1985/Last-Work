58b163ba19acc5ff6ae17b77f7e18254efa80dd9
Вересков Андрей <vereskov1985@mail.ru>
2023-01-14 19:46:47 +0300

    second commit

--- LastTask.cs
+++ LastTask.cs
@@ -10,6 +10,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string PrintM(string[] array)
+            {
+                string result = string.Empty;
+                result = "[ ";
+                for (int i = 0; i < array.Length; i++)
+                {
+                    result += $"{array[i],1}";
+                    if (i < array.Length - 1) result += ", ";
+                }
+                result += " ]";
+                return result;
+            }
+
+            int InputN(string input)
+            {
+                Console.Write(input);
+                int outt = Convert.ToInt32(Console.ReadLine());
+                return outt;
+            }
 
 
 
